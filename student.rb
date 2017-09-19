56ff34a926cb6fcfc54c639885cc722b0b2abf11
Jett Crowson <jettcrowson@gmail.com>
2017-09-19 16:29:12 -0600

    Change students to objects

---
+++ student.rb
@@ -0,0 +1,27 @@
+class Student
+
+    def initialize(name, eyeColor, age, height, bloodtype)
+    @name = name
+    @eyeColor = eyeColor
+    @age = age
+    @height = height
+    @bloodtype = bloodtype
+    end
+    def name
+        return @name
+    end
+    def eyeColor
+        return @eyeColor
+    end
+    def age
+        return @age
+    end
+    def height
+        return @height
+    end
+    def bloodtype
+        return @bloodtype
+    end
+
+
+end
--- students.rb
+++ students.rb
@@ -1,297 +1,174 @@
+require_relative "student"
+
 students = []
-eyeColors = []
-ages = []
-heights = []
-bloodtypes = []
 
-File.open("./student_data.csv").each do |line|
+File.open("./student_data.csv").each_with_index do |line, i|
     info = line.split(",")
-    students.push(info[0].strip)
-    eyeColors.push(info[1].strip)
-    ages.push(info[2].strip.to_i)
-    heights.push([info[3].strip.to_i, info[4].strip.to_i])
-    bloodtypes.push(info[5].strip)
+    students[i] = Student.new(info[0].strip, info[1].strip, info[2].strip.to_i, [info[3].strip.to_i, info[4].strip.to_i], info[5].strip) 
 end
 
-def eyeColorStudents(color, eyeColorArray, studentArray, ageArray)
+def fetchAttribute(objList, attribute)
+    attributeList = []
 
-    studentColors = []
-    studentIndexes = []
-    studentAges = []
-
-    eyeColorArray.each_with_index do |eyeColor, i|
-
-        if eyeColor.downcase == color.downcase
-
-            studentColors.push(studentArray[i])
-            studentIndexes.push(i)
-
-        end    
-
+    objList.each do |obj|
+        attributeList.push(obj.public_send(attribute))
     end
 
-    studentIndexes.each do |i|
-
-        studentAges.push(ageArray[i])
-
-    end
-
-    return [studentColors.length, studentColors, studentIndexes, studentAges]
+    return attributeList
 end
 
-def oldEnoughToDrive(ageArray,studentArray)
-
-    oldEnough = []
-
-    ageArray.each_with_index do |age, i|
-
-        if age >= 16
-
-            oldEnough.push(studentArray[i])
-
+def eyeColorStudents(color, students)
+    studentsWithEyeColor = []    
+        students.each_with_index do |student, i|
+            if student.eyeColor.downcase == color.downcase
+                studentsWithEyeColor.push(student)
+            end    
         end
+        return [studentsWithEyeColor.length, studentsWithEyeColor]
+end
 
-    end
-
+def oldEnoughToDrive(students)
+    oldEnough = []
+        students.each_with_index do |student, i|
+            if student.age >= 16
+                oldEnough.push(student.name)
+            end
+        end
     return oldEnough
 end
 
 def vowelCounter(word)
-
-    vowels = 0
-    vowelList = ["a", "e", "i", "o", "u"]
-
-    word.split("").each do |letter|
-
-        if vowelList.include?(letter.downcase)
-
-            vowels += 1
-
+        vowels = 0
+        vowelList = ["a", "e", "i", "o", "u"]
+        word.split("").each do |letter|
+            if vowelList.include?(letter.downcase)
+                vowels += 1
+            end
         end
-
-    end
-
     return vowels
 end
 
-def studentsOfAge(age, ageArray, studentArray)
-
-    studentsMatchingAge = []
-
-    ageArray.each_with_index do |age,i|
-
-        if age == 15
-
-            studentsMatchingAge.push(studentArray[i])
-
+def studentsOfAge(age, students)
+        studentsMatchingAge = []
+        students.each_with_index do |student,i|
+            if student.age == age
+                studentsMatchingAge.push(student)
+            end
         end
-
-    end
-
     return studentsMatchingAge
 end
 
-def mostVowelsSophomores(ageArray, sudentsArray)
-
-    longest = ""
-
-    studentsOfAge(15, ageArray, sudentsArray).each do |name|
-
-        if vowelCounter(name) > vowelCounter(longest)
-
-            longest = name
-
+def mostVowelsSophomores(students)
+        longest = ""
+        studentsOfAge(15, students).each do |student|
+            if vowelCounter(student.name) > vowelCounter(longest)
+                longest = student.name
+            end
         end
-        
-    end
-
     return longest
 end
 
 def toInches(heightArray)
-
     return (heightArray[0] * 12) + heightArray[1]
 end
 
-def averageHeight(heightArray)
-
+def average(list)
     total = 0.0
-    numberStudents = 0
-
-    heightArray.each do |heightArray|
-
-        total += toInches(heightArray)
-        numberStudents += 1
-
+    list.each do |item|
+        total += item
     end
-
-    return total / numberStudents
+    return total / list.length
 end
 
-def averageAge(ageArr)
-
-    total = 0.0
-    numberStudents = 0
-
-    ageArr.each do |ageArray|
-
-        total += ageArray
-        numberStudents += 1
-
-    end
-
-    return total / numberStudents
+def averageHeight(students)
+    heights = []
+    students.each{ |student| heights.push(toInches(student.height))} 
+    return average(heights)
 end
 
-def findAges(studentIndexes, ageArray)
-
-    ageList = []
-
-    studentIndexes.each do |i|
-
-        ageList.push(ageArray[i])
-
-    end
-
-    return ageList
+def averageAge(students)
+    ages = []
+    students.each{ |student| ages.push(student.age)} 
+    return average(ages)
 end
-
-def getNames(students, indexes)
-
-    studentList = []
-    
-    indexes.each do |i|
-
-        studentList.push(students[i])
-
-    end
-
-    return studentList
-end
-
-def closestToAgeByEyeColor(color, eyeColors, students, ages)
-
-    closestStudents = []
-    average = averageAge(findAges(eyeColorStudents(color, eyeColors, students, ages)[2], ages))
-    studentsI = eyeColorStudents(color, eyeColors, students, ages)[2]
-    studentsNames = getNames(students, studentsI)
-    closest = [0]
-
-
-    studentsI.each do |i|
-
-        if (average - ages[i]).abs < (average - ages[closest[0]]).abs
-
-            closest = []
-            closest.push(i)
-
-        elsif (average - ages[i]).abs == (average - ages[closest[0]]).abs
-
-            closest.push(i)
-
+def closestToAgeByEyeColor(color, students)
+        closestStudents = []
+        average = averageAge(eyeColorStudents(color, students)[1])
+        studentsList = eyeColorStudents(color, students)[1]
+        ages = fetchAttribute(students, "age")
+        closest = [studentsList[0]]
+        studentsList.each do |student|
+            if (average - student.age).abs < (average - closest[0].age).abs
+                closest = []
+                closest.push(student)
+            elsif (average - student.age).abs == (average - closest[0].age).abs
+                closest.push(student)
+            end
         end
-
-    end
-    
-    return getNames(students, closest)
+    return fetchAttribute(closest, "name")
 end
 
 def donatableBloodTypes(bloodtype)
-
     case bloodtype
-
     when "A"
-
         return ["O", "A"]
-
     when "B"
-
         return ["O", "B"]
-
     when "O"
-
         return ["O"]
-
     else
         return ["AB", "O", "B", "A"]
-
     end
-
 end
 
-def nameToBloodtype(name, bloodList, students)
-
+def nameToBloodtype(name, students)
     students.each_with_index do |student, i|
-
-        if student == name
-
-            return bloodList[i]
-
+        if student.name == name
+            return student.bloodtype
         end
-
     end
-
     return "O"
 end
 
-def bloodStudents(students, bloodList, desiredBloodtype)
-
-    donors = []
-
-    donatableBloodTypes(desiredBloodtype).each do |types|
-        
-        students.each do |name|
-
-            if types == nameToBloodtype(name, bloodList, students)
-
-                donors.push(name)
-
+def bloodStudents(students, desiredBloodtype)
+        donors = []
+        donatableBloodTypes(desiredBloodtype).each do |types|
+            
+            students.each do |student|
+                if types == nameToBloodtype(student.name, students)
+                    donors.push(student)
+                end
             end
-
         end
-
-    end
-
     return donors
 end
 
-def mostDonors(students, bloodtypes)
-
-    mostDonors = []
-    donorNumber = 0
-
-    students.each do |name|
-
-        if bloodStudents(students, bloodtypes, nameToBloodtype(name, bloodtypes, students)).length > donorNumber
-
-            mostDonors = [name]
-            donorNumber = bloodStudents(students, bloodtypes, nameToBloodtype(name, bloodtypes, students)).length
-
-        elsif bloodStudents(students, bloodtypes, nameToBloodtype(name, bloodtypes, students)).length == donorNumber
-
-            mostDonors.push(name)
-
+def mostDonors(students)
+        mostDonors = []
+        donorNumber = 0
+        students.each do |student|
+            if bloodStudents(students, nameToBloodtype(student.name, students)).length > donorNumber
+                mostDonors = [student]
+                donorNumber = bloodStudents(students, nameToBloodtype(student.name, students)).length
+            elsif bloodStudents(students, nameToBloodtype(student.name, students)).length == donorNumber
+                mostDonors.push(student)
+            end
         end
-        
-    end
-
     return [mostDonors, donorNumber]
 end
 
 def writeToFile(content, name)
-
-    outFile = File.new("#{name}.txt", "w")
-    outFile.puts(content)
-    outFile.close
-
+        outFile = File.new("#{name}.txt", "w")
+        outFile.puts(content)
+        outFile.close
 end
 
 def secondLetterSort(students)
-    orderedNames = [students[0]]
+    orderedNames = [students[0].name]
     secondLetterNames = []
     putFirstBack = []
 
-    students.each{ |student| secondLetterNames.push(student.split("").drop(1).push(student[0]).join(""))}
+    students.each{ |student| secondLetterNames.push(student.name.split("").drop(1).push(student.name[0]).join(""))}
 
     secondLetterNames.sort.each{ |name| putFirstBack.push(name.split("").take(name.length-1).unshift(name[name.length-1]).join(""))}
 
@@ -299,57 +176,40 @@ def secondLetterSort(students)
 end
 
 puts "-----"
-
 puts "How many students have brown eyes?"
-puts eyeColorStudents("brown", eyeColors, students, ages)[0]
-
+puts eyeColorStudents("brown", students)[0]
 puts "-----"
-
 puts "List of students old enough to drive"
-puts oldEnoughToDrive(ages, students)
-
+puts oldEnoughToDrive(students)
 puts "-----"
-
-puts "List of green eyed girls"
-puts eyeColorStudents("green", eyeColors, students, ages)[1]
-
+puts "List of green eyed students"
+puts fetchAttribute(eyeColorStudents("green", students)[1], "name")
 puts "-----"
-
 puts "Which 15 year old has the most vowels in their name?"
-puts mostVowelsSophomores(ages, students)
-
+puts mostVowelsSophomores(students)
 puts "-----"
-
 puts "What is the average height of the students?"
-puts averageHeight(heights)
-
+puts averageHeight(students)
 puts "-----"
-
 puts "What is the average age of green eyed students?"
-puts averageAge(findAges(eyeColorStudents("green", eyeColors, students, ages)[2], ages))
-
+puts averageAge(eyeColorStudents("green", students)[1])
 puts "-----"
-
 puts "Which green eyed students are closest to the average?"
-puts closestToAgeByEyeColor("green", eyeColors, students, ages)
-
+puts closestToAgeByEyeColor("green", students)
 puts "-----"
-
 puts "What bloodtype can the student recieve dontations from?"
-puts donatableBloodTypes(nameToBloodtype("Alice", bloodtypes, students))
-
+puts donatableBloodTypes(nameToBloodtype("Alice", students))
 puts "-----"
-
 puts "Which students can donate to our student?"
-puts bloodStudents(students, bloodtypes, nameToBloodtype("Alice", bloodtypes, students))
-
+puts fetchAttribute(bloodStudents(students, nameToBloodtype("Alice", students)), "name")
 puts "-----"
-
 puts "The students with the most donors were"
-puts mostDonors(students, bloodtypes)[0]
+puts fetchAttribute(mostDonors(students)[0], "name")
 puts "With this many donors each"
-puts mostDonors(students, bloodtypes)[1]
-writeToFile(mostDonors(students, bloodtypes), "problem7")
-
-puts "Sort the students in alphebetical order according to the second letter of their names."
+puts mostDonors(students)[1]
+puts "-----"
+puts "Sort the students in alphebetical order according to the second letter of their names, ignoring the first."
 puts secondLetterSort(students)
+
+writeToFile(fetchAttribute(mostDonors(students)[0], "name"), "problem7")
+
